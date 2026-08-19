80f04898cee176551499cb3fb49c2191c83bf8fb
dilusharadeeshan <dilusharadeeshan@gmail.com>
2026-08-19 17:22:24 +0530

    add update feature

--- backend/routes/studentroutes.js
+++ backend/routes/studentroutes.js
@@ -26,5 +26,34 @@ router.get("/", async (req, res) => {
   }
 });
 
+router.get("/:id", async (req, res) => {
+  try {
+    const student = await Student.findById(req.params.id);    
+    
+    res.status(200).json(student);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message
+    });
+  }
+});
+
+
+router.put("/:id", async (req, res) => {
+  try {
+    const student = await Student.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+
+    res.status(200).json(student);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message
+    });
+  }
+});
+
 export default router;
 
